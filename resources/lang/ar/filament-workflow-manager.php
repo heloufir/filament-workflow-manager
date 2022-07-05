14b78c249f585d0c998e7c864cf8d8201b6e8acc
EL OUFIR Hatim <eloufirhatim@gmail.com>
2022-07-05 11:34:31 +0100

    Add translations (fr / ar)

---
+++ resources/lang/ar/filament-workflow-manager.php
@@ -0,0 +1,78 @@
+<?php
+
+return [
+    'resources' => [
+        'workflow' => [
+            'table' => [
+                'name' => 'الاسم',
+                'model' => 'النموذج'
+            ],
+            'page' => [
+                'edit' => [
+                    'workflow' => 'إدارة سير العمل'
+                ],
+                'workflow' => [
+                    'heading' => 'انتقالات سير العمل',
+                    'actions' => [
+                        'add_status' => 'حالة جديدة'
+                    ],
+                    'modal' => [
+                        'edit' => [
+                            'title' => 'تعديل الانتقال',
+                            'form' => [
+                                'status_from' => 'من الوضع',
+                                'status_to' => 'إلى الوضع',
+                                'status_from_color' => 'لون حالة المصدر',
+                                'status_to_color' => 'لون حالة الوجهة',
+                                'submit' => 'تعديل',
+                                'cancel' => 'الغاء',
+                            ],
+                            'messages' => [
+                                'submitted' => 'انتقال مسجل !'
+                            ],
+                        ],
+                        'delete' => [
+                            'title' => 'حذف الانتقال',
+                            'buttons' => [
+                                'submit' => 'حذف',
+                                'cancel' => 'الغاء',
+                            ],
+                            'messages' => [
+                                'confirmation_title' => 'هل أنت متأكد أنك تريد حذف هذا الانتقال ?',
+                                'deleted' => 'تمت إزالة الانتقال !'
+                            ],
+                        ],
+                        'add' => [
+                            'title' => 'أضف الانتقال',
+                            'form' => [
+                                'status_from' => 'من الوضع',
+                                'status_to' => 'إلى الوضع',
+                                'status_from_color' => 'لون حالة المصدر',
+                                'status_to_color' => 'لون حالة الوجهة',
+                                'submit' => 'أضف',
+                                'cancel' => 'الغاء',
+                            ],
+                            'messages' => [
+                                'duplicated' => 'الانتقال موجود !',
+                                'submitted' => 'تمت إضافة الانتقال !'
+                            ],
+                        ],
+                        'add_status' => [
+                            'title' => 'أضف الحالة',
+                            'form' => [
+                                'name' => 'الاسم',
+                                'color' => 'اللون',
+                                'submit' => 'أضف',
+                                'cancel' => 'الغاء',
+                            ],
+                            'messages' => [
+                                'duplicated' => 'الحالة موجودة !',
+                                'submitted' => 'تمت إضافة الحالة !'
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ]
+];
---
+++ resources/lang/fr/filament-workflow-manager.php
@@ -0,0 +1,78 @@
+<?php
+
+return [
+    'resources' => [
+        'workflow' => [
+            'table' => [
+                'name' => 'Nom',
+                'model' => 'Modèle'
+            ],
+            'page' => [
+                'edit' => [
+                    'workflow' => 'Gérer le flux de travail'
+                ],
+                'workflow' => [
+                    'heading' => 'Transitions de flux de travail',
+                    'actions' => [
+                        'add_status' => 'Nouveau statut'
+                    ],
+                    'modal' => [
+                        'edit' => [
+                            'title' => 'Modifier la transition',
+                            'form' => [
+                                'status_from' => 'Depuis le statut',
+                                'status_to' => 'Vers le statut',
+                                'status_from_color' => 'Couleur du statut source',
+                                'status_to_color' => 'Couleur du statut destination',
+                                'submit' => 'Modifier',
+                                'cancel' => 'Annuler',
+                            ],
+                            'messages' => [
+                                'submitted' => 'Transition enregistrée !'
+                            ],
+                        ],
+                        'delete' => [
+                            'title' => 'Supprimer la transition',
+                            'buttons' => [
+                                'submit' => 'Supprimer',
+                                'cancel' => 'Annuler',
+                            ],
+                            'messages' => [
+                                'confirmation_title' => 'Voulez-vous vraiment supprimer cette transition ?',
+                                'deleted' => 'Transition supprimée !'
+                            ],
+                        ],
+                        'add' => [
+                            'title' => 'Ajouter une transition',
+                            'form' => [
+                                'status_from' => 'Depuis le statut',
+                                'status_to' => 'Vers le statut',
+                                'status_from_color' => 'Couleur du statut source',
+                                'status_to_color' => 'Couleur du statut destination',
+                                'submit' => 'Ajouter',
+                                'cancel' => 'Annuler',
+                            ],
+                            'messages' => [
+                                'duplicated' => 'La transition existe déjà !',
+                                'submitted' => 'Transition ajoutée !'
+                            ],
+                        ],
+                        'add_status' => [
+                            'title' => 'Ajouter un statut',
+                            'form' => [
+                                'name' => 'Nom',
+                                'color' => 'Couleur',
+                                'submit' => 'Ajouter',
+                                'cancel' => 'Annuler',
+                            ],
+                            'messages' => [
+                                'duplicated' => 'Le statut existe déjà !',
+                                'submitted' => 'Status ajoutée !'
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ]
+];
