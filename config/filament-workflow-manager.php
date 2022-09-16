ad8909a1f834847694b6aa2e5d5fca5158cb5430
HUSAM TAREQ AL-SHAIBANI <hu22am1@gmail.com>
2022-09-16 20:19:20 +0300

    Add the navigation config

--- config/filament-workflow-manager.php
+++ config/filament-workflow-manager.php
@@ -38,6 +38,39 @@ return [
     */
 
     'user_name' => 'name',
+    
+    /*
+    |--------------------------------------------------------------------------
+    |  Navigation Group Name
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the navigation group name (by default "Settings")
+    |
+    */
+
+    'navigation_group' => 'Settings',
+    
+    /*
+    |--------------------------------------------------------------------------
+    |  Navigation Sort
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the navigation sort number (by default 1)
+    |
+    */
+
+    'navigation_sort' => 1,
+    
+    /*
+    |--------------------------------------------------------------------------
+    |  Navigation Icon
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the navigation icon class (by default "heroicon-o-collection")
+    |
+    */
+
+    'navigation_icon' => 'heroicon-o-collection',
 
     /*
     |--------------------------------------------------------------------------
